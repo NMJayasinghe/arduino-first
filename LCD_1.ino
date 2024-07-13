ffbc70c5b2e0fb0ff1feb036b622057a25dab5bb
NMJayasinghe <nethminimadara2058@gmail.com>
2024-07-13 23:54:55 +0530

    Blinking the LCD display

--- LCD_1.ino
+++ LCD_1.ino
@@ -1,21 +1,23 @@
 #include<Wire.h>
 #include<LiquidCrystal_I2C.h>
-LiquidCrystal_I2C LCD(0x27,16,2);
+LiquidCrystal_I2C LCD(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 void setup() {
-
+//main code of setup
   LCD.begin(16,2);
-  LCD.noBacklight();
+  LCD.backlight();
   
 }
 
 void loop() {
+  //Display words
   LCD.setCursor(3,0);
   LCD.print("Loading...");
   delay(1000);
 
-  /*LCD.setCursor(6,1);
-  LCD.print("Shirodhara");
-  delay(1000);*/
-
+// Blinking dispaly
+ LCD.noDisplay();
+  delay(500);
+  LCD.display();
+  delay(500);
 }
